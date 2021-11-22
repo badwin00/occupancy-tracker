dc51670800bcd3a02df61274962c8c5ad1749044
Sam Goodwin <sam.windoog@gmail.com>
2021-11-22 17:53:13 -0500

    got rid of unittest because it sucks, added occupancy test

--- sense.py
+++ sense.py
@@ -2,11 +2,12 @@ import RPi.GPIO as GPIO
 import IPython
 import time
 import firebase_admin
-import unittest
 import requests
 from datetime import datetime
 from firebase_admin import credentials
 from firebase_admin import db
+RUN_TESTS = 1 # toggle to run tests
+occupancy = 0
 
 # set up GPIO pins
 GPIO.setmode(GPIO.BCM)
@@ -26,24 +27,36 @@ def blink():
 
 def report():
     # get the current occupancy mappings and find the most recent one so we can increment its occupancy and send it back
+    if RUN_TESTS:
+        global occupancy
+        occupancy += 1
     current = ref.get()
     current[datetime.now().strftime("%Y-%m-%d-%H")] = current.get(datetime.now().strftime("%Y-%m-%d-%H"),0)+1
     ref.set(current)
+
+# check if website's occupancy matches the occupancy measured locally on the pi
+def test_occupancy():
+    ref = db.reference(path="Time",url='https://capacity-tracker-80518-default-rtdb.firebaseio.com/')
+    current = ref.get().get(datetime.now().strftime("%Y-%m-%d-%H"),0) 
+    assert current == occupancy, "Database and local occupancy out of sync! Expected: " + str(occupancy) + " got: " + str(current)
+
+# check if the website responds to a request properly
+def test_up():
+    r = requests.get('https://badwin00.github.io/occupancy-tracker/')
+    assert r.status_code == 200, "Unacceptable response code: " + r.status_code
 
 while 1:
     if GPIO.input(23) == True:
         print("motion sensed!")
         report()
         blink()
+        if RUN_TESTS:
+            test_up()
+            test_occupancy()
 
-class websync_test(unittest.TestCase):
-    def test_occupancy(self):
-        # check if website's occupancy matches the occupancy measured locally on the pi
-        pass
-
-    def test_up(self):
-        # check if the website responds to a request properly
-        r = requests.get('https://badwin00.github.io/occupancy-tracker/')
-        self.assertEqual(r.status_code,200,"Unacceptable response code: " + r.status_code)
+test_up()
+print("test_up PASSED!")
+test_occupancy()
+print("test_occupancy PASSED!")
 
 
